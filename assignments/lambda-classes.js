40f1424416cd6cff993a675c1622283fe888c871
Lorenzo Simpson <lorenzosimp@gmail.com>
2019-08-22 15:35:20 -0400

    complete person class

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,11 @@
 // CODE here for your Lambda Classes
+class Person {
+    constructor(personAttrs) {
+        this.newName = personAttrs.name;
+        this.newAge = personAttrs.age;
+        this.newLocation = personAttrs.location;
+    }
+    speak() {
+        return `Hello my name is ${this.newName}, I am from ${this.newLocation}`;
+    };
+};
